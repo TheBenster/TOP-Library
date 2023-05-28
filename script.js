d7162dbbfbb28d3e84b11ceb8ac6df3e8298e0ca
TheBenster <105664089+TheBenster@users.noreply.github.com>
2023-05-28 17:32:13 -0400

    add some elements to js, incorporate dialog element

--- script.js
+++ script.js
@@ -1,6 +1,30 @@
 card = document.querySelector('.book')
 console.log(card)
+container = document.querySelector('.container')
+let para = document.createElement('p')
+let modal = document.getElementById('#myModal');
+let modalBtn = document.getElementById('modalBtn');
+var span = document.getElementsByClassName("close")[0];
+let form = document.querySelector('form')
+let testModal = document.querySelector('#test')
+let cancel = document.getElementById('#cancel')
 
+cancel.addEventListener('click', ()=>{
+    testModal.close();
+    alert('test')
+})
+modalBtn.addEventListener('click', ()=>{
+    testModal.showModal();
+    
+})
+
+
+
+window.onclick = function(event) {
+    if (event.target == modal) {
+      modal.style.display = "none";
+    }
+  }
 
 bloodMeredian = new Book('Blood Meridian', 'Cormac McCarthy', 340, true);
 eastOfEden = new Book('East of Eden', 'John Steinbeck', 748, true);
@@ -13,13 +37,10 @@ myLibrary = [bloodMeredian, eastOfEden, cosmos, slaughterHouseFive];
 
 
 function displayBook(myLibrary){
+   
     myLibrary.forEach(book => {
-        book.forEach(()=>{
-            book.textContent = 'atg'
-        }
-        )
-        card.textContent = book.title
-        console.log(book)
+        para.textContent = book.title;
+        book.appendChild(para)        
     });
 }
 function Book(title, author, pages, read){
