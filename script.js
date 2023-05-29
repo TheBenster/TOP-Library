6623c00148ee18211c6d8bbb79c82780dc3f2aff
TheBenster <105664089+TheBenster@users.noreply.github.com>
2023-05-29 17:47:32 -0400

    Add functionality to the modal, style everything pretty, set the form values to equal variables, create function to display variables on a card, and add a delete function. Basically complete the project.

--- script.js
+++ script.js
@@ -1,55 +1,93 @@
-card = document.querySelector('.book')
-console.log(card)
-container = document.querySelector('.container')
-let para = document.createElement('p')
-let modal = document.getElementById('#myModal');
+let modal = document.getElementById('myModal');
 let modalBtn = document.getElementById('modalBtn');
 var span = document.getElementsByClassName("close")[0];
-let form = document.querySelector('form')
-let testModal = document.querySelector('#test')
-let cancel = document.getElementById('#cancel')
+let form = document.querySelector('form');
+let testModal = document.getElementById('modal');
+let cancel = document.getElementById('close');
+let submit = document.getElementById('submit');
+let titleEntry = document.querySelector('#title');
+let authorEntry = document.querySelector('#author');
+let pagesEntry = document.querySelector('#pages');
+let readEntry = document.querySelector('#read');
 
-cancel.addEventListener('click', ()=>{
-    testModal.close();
-    alert('test')
-})
+
 modalBtn.addEventListener('click', ()=>{
-    testModal.showModal();
+    testModal.setAttribute('open', 'true')
     
 })
+cancel.addEventListener('click', ()=>{
+    testModal.removeAttribute('open')
+})
+submit.addEventListener('click', (event)=>{
+    event.preventDefault();
+    addBookToLibrary()
+    displayBook(myLibrary);
+    testModal.removeAttribute('open')
+})
 
 
 
-window.onclick = function(event) {
-    if (event.target == modal) {
-      modal.style.display = "none";
-    }
-  }
-
-bloodMeredian = new Book('Blood Meridian', 'Cormac McCarthy', 340, true);
-eastOfEden = new Book('East of Eden', 'John Steinbeck', 748, true);
-cosmos = new Book('Cosmos', 'Carl Sagan', 250, true);
-slaughterHouseFive = new Book('Slaughter House Five', 'Kurt Vonnegut', 150, true);
-
-
-myLibrary = [bloodMeredian, eastOfEden, cosmos, slaughterHouseFive];
-
-
-
-function displayBook(myLibrary){
-   
-    myLibrary.forEach(book => {
-        para.textContent = book.title;
-        book.appendChild(para)        
-    });
+  let list = document.querySelector('.book-list');
+function addBookToLibrary(){
+    let title = titleEntry.value;
+    let author = authorEntry.value;
+    let pages = pagesEntry.value;
+    let read = readEntry.value;
+    let newBook = new Book(title, author, pages, read);
+    myLibrary.push(newBook)
 }
-function Book(title, author, pages, read){
+
+function displayBook(myLibrary) {
+list.innerHTML = ''; // Clear previous content
+  
+myLibrary.forEach(book => {
+    let card = document.createElement('div');
+    card.classList.add('book')
+    let titleDiv = document.createElement('div')
+    let authorDiv = document.createElement('div')
+    let readDiv = document.createElement('div')
+    let pagesDiv = document.createElement('div')
+    let delBTN = document.createElement('button')
+    delBTN.textContent = 'Delete Book'
+    delBTN.classList.add('del')
+    delBTN.addEventListener('click', ()=>{
+        card.remove();
+    })
+    //   card.textContent = `Title: ${book.title} Author: ${book.author} Read: ${book.read} Pages: ${book.pages}`
+    list.appendChild(card);
+    titleDiv.textContent = `Title: ${book.title}`
+    authorDiv.textContent = `Author: ${book.author}`
+    if(book.read == true){
+        readDiv.textContent = 'Finished: Yes.';
+    } else{
+        readDiv.textContent = 'Finished: Not yet.'
+    }
+    // readDiv.textContent = `Finished: ${book.read}`
+    pagesDiv.textContent = `Pages: ${book.pages}`
+
+    
+      
+    card.appendChild(titleDiv)
+    card.appendChild(authorDiv)
+    card.appendChild(pagesDiv)
+    card.appendChild(readDiv)
+    card.appendChild(delBTN)
+});
+}
+  
+function Book(title, author, pages, read) {
     this.title = title;
     this.author = author;
     this.pages = pages;
     this.read = read;
 }
-
-
-
-displayBook(myLibrary)
+  
+let bloodMeridian = new Book('Blood Meridian', 'Cormac McCarthy', 340, true);
+let eastOfEden = new Book('East of Eden', 'John Steinbeck', 748, true);
+let cosmos = new Book('Cosmos', 'Carl Sagan', 250, true);
+let slaughterHouseFive = new Book('Slaughter House Five', 'Kurt Vonnegut', 150, true);
+  
+let myLibrary = [bloodMeridian, eastOfEden, cosmos, slaughterHouseFive];
+  
+displayBook(myLibrary);
+  
