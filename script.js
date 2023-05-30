51dcbcf0d20cbb84bea4f7998930902e04546c4d
TheBenster <105664089+TheBenster@users.noreply.github.com>
2023-05-30 18:03:55 -0400

    fix silly checkbox logic.

--- script.js
+++ script.js
@@ -1,7 +1,4 @@
-let modal = document.getElementById('myModal');
 let modalBtn = document.getElementById('modalBtn');
-var span = document.getElementsByClassName("close")[0];
-let form = document.querySelector('form');
 let testModal = document.getElementById('modal');
 let cancel = document.getElementById('close');
 let submit = document.getElementById('submit');
@@ -9,6 +6,7 @@ let titleEntry = document.querySelector('#title');
 let authorEntry = document.querySelector('#author');
 let pagesEntry = document.querySelector('#pages');
 let readEntry = document.querySelector('#read');
+let readChecked = document.querySelector('#read').checked
 
 
 modalBtn.addEventListener('click', ()=>{
@@ -19,7 +17,11 @@ cancel.addEventListener('click', ()=>{
     testModal.removeAttribute('open')
 })
 submit.addEventListener('click', (event)=>{
+    console.log(readChecked)
     event.preventDefault();
+    if(titleEntry.length == 0){
+        titleEntry.textContent = "you can't leave this blank sir"
+    }
     addBookToLibrary()
     displayBook(myLibrary);
     testModal.removeAttribute('open')
@@ -27,21 +29,31 @@ submit.addEventListener('click', (event)=>{
 
 
 
-  let list = document.querySelector('.book-list');
+let list = document.querySelector('.book-list');
+
+// Assigns form values to variables and then instantiates to Book, and finally pushes to array.
+// This is needed because the variables needs a way to save the values on the form.
 function addBookToLibrary(){
     let title = titleEntry.value;
     let author = authorEntry.value;
     let pages = pagesEntry.value;
     let read = readEntry.value;
+    if(title === ''||author === '' || pages === '' || read === ''){
+        myLibrary.pop(newBook)
+    }
     let newBook = new Book(title, author, pages, read);
     myLibrary.push(newBook)
 }
 
+// This creates a div containing the variables above ^^^ 
+// The reason for so many divs is to clean up what would otherwise be a jumbled mess.
 function displayBook(myLibrary) {
-list.innerHTML = ''; // Clear previous content
+list.innerHTML = ''; // clears previous content
   
 myLibrary.forEach(book => {
     let card = document.createElement('div');
+
+
     card.classList.add('book')
     let titleDiv = document.createElement('div')
     let authorDiv = document.createElement('div')
@@ -53,15 +65,14 @@ myLibrary.forEach(book => {
     delBTN.addEventListener('click', ()=>{
         card.remove();
     })
-    //   card.textContent = `Title: ${book.title} Author: ${book.author} Read: ${book.read} Pages: ${book.pages}`
+    var x = document. createElement("INPUT"); x. setAttribute("type", "checkbox"); 
     list.appendChild(card);
     titleDiv.textContent = `Title: ${book.title}`
     authorDiv.textContent = `Author: ${book.author}`
-    if(book.read == true){
-        readDiv.textContent = 'Finished: Yes.';
-    } else{
-        readDiv.textContent = 'Finished: Not yet.'
-    }
+    // For some reason if i didn't make this if statement, and i didn't read the book it would be "Finished: on"
+    readDiv.textContent = 'Finished:'
+
+    
     // readDiv.textContent = `Finished: ${book.read}`
     pagesDiv.textContent = `Pages: ${book.pages}`
 
@@ -71,10 +82,12 @@ myLibrary.forEach(book => {
     card.appendChild(authorDiv)
     card.appendChild(pagesDiv)
     card.appendChild(readDiv)
+    card.appendChild(x)
     card.appendChild(delBTN)
 });
 }
-  
+
+
 function Book(title, author, pages, read) {
     this.title = title;
     this.author = author;
